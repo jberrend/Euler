1074c3f96fca29cbb4cdbc8b2ae4021387d015cb
Qwertee <mobiusdragon96@gmail.com>
2018-03-03 19:23:31 -0600

    new solutions - new languages

--- Elixir/euler/lib/euler.ex
+++ Elixir/euler/lib/euler.ex
@@ -1,22 +1,17 @@
 defmodule Euler do
-  @moduledoc """
-  Documentation for Euler.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Euler.hello
-      :world
-
-  """
   def problem16 do
     num = Kernel.trunc(:math.pow(2, 1000))
 
     Enum.reduce(Integer.digits(num, 10), 0, fn x, acc ->
       x + acc
     end)
+  end
+
+  def problem29 do
+    for a <- 2..100,
+        b <- 2..100 do
+          :math.pow(a, b)
+    end
+    |> MapSet.new |> MapSet.size
   end
 end
